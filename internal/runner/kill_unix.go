dce5df7ea02176b5f518bb01512a0e44220c2512
Bala <rbalavignesh06@gmail.com>
2026-03-06 22:03:52 +0530

    feat: keep previous server alive on build failure

---
+++ internal/runner/kill_unix.go
@@ -0,0 +1,15 @@
+/*package runner
+
+import (
+	"os/exec"
+	"syscall"
+)
+
+func setSysProcAttr(cmd *exec.Cmd) {
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+}
+
+func killProcess(cmd *exec.Cmd) {
+	syscall.Kill(-cmd.Process.Pid, syscall.SIGKILL)
+}
+*/
